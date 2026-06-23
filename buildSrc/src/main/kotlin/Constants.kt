619e3c1cdeecfbfe22711ad4663752acec84d4f2
zaneschepke <dev@zaneschepke.com>
2026-06-23 12:51:01 -0400

    chore: release 5.0.5

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 object Constants {
-    const val VERSION_NAME = "5.0.4"
-    const val VERSION_CODE = 50004
+    const val VERSION_NAME = "5.0.5"
+    const val VERSION_CODE = 50005
     const val TARGET_SDK = 37
     const val MIN_SDK = 26
 
